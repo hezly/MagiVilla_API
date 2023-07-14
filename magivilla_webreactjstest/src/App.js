1d84c2e26a9eec224e062d8e5a32a5f074a50099
Hezly <32444835+hezly@users.noreply.github.com>
2023-07-14 14:25:07 +0700

    update

--- magivilla_webreactjstest/src/App.js
+++ magivilla_webreactjstest/src/App.js
@@ -1,25 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import VillaCard from './VillaCard';
 
 const API_URL = 'https://localhost:7001/api/v1/VillaAPI';
 
 function App() {
+  const [villas, setVillas] = useState([]);
   const GetVillas = async () =>{
-    const response = await fetch(API_URL)
-    // .then((data) => {
-    //   console.log(data.json());
-    // }).catch(error => {
-    //   console.log(error);
-    // })
-    ;
+    const response = await fetch(API_URL);
     const data = await response.json();
     console.log(data.result);
+    setVillas(data.result);
   }
   useEffect(() => {
     GetVillas();
   },[]);
   return (
     <div className="App">
-      LOL
+      {
+        villas?.length > 0
+          ? (
+            <div className='container'>
+              {
+                villas.map((villa) => (
+                  <VillaCard villa={villa}/>
+                ))
+              }
+            </div>
+          ) : (
+            <div className='empty'>
+              NOT FOUND
+            </div>
+          )
+      }
     </div>
   );
 }
---
+++ magivilla_webreactjstest/src/VillaCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const VillaCard = ({villa}) => {
+    return (
+        <div className='villa'>
+            <div>
+                <div>
+                    <img src={villa.ImageUrl} alt='villa-img'></img>
+                </div>
+                <div>
+                    <p>{villa.Name}</p>
+                    <p>{villa.Details}</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <a >content</a>
+                </div>
+                <div>
+                    <span >Max Occupancy : {villa.Occupancy} adults</span>
+                    <br />
+                    <span>Villa Size : {villa.Sqft} sqft</span>
+                    <br />
+                </div>
+                <div>
+                    <span >
+                        <span >
+                            {villa.Rate}
+                        </span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default VillaCard;
